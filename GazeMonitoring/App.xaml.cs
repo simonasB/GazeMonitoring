6ff6ed494e0869a93eddbacd9d7f8805ddf6470b
Simonas Baltulionis <simonas.baltulionis@virtustream.com>
2018-04-16 21:01:10 +0300

    Launch wpf as tray application

--- GazeMonitoring/App.xaml.cs
+++ GazeMonitoring/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace GazeMonitoring
 {
@@ -7,5 +8,20 @@ namespace GazeMonitoring
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
     }
 }
--- TheEyeTribeMonitoring/EyeTribeModule.cs
+++ TheEyeTribeMonitoring/EyeTribeModule.cs
@@ -1,9 +1,11 @@
 ﻿using Autofac;
+using EyeTribe.ClientSdk;
 using GazeMonitoring.Common;
 
 namespace TheEyeTribeMonitoring {
     public class EyeTribeModule : Module {
         protected override void Load(ContainerBuilder builder) {
+            GazeManager.Instance.Activate(GazeManagerCore.ApiVersion.VERSION_1_0);
             builder.RegisterType<EyeTribeGazePointStreamFactory>().As<IGazePointStreamFactory>();
         }
     }
