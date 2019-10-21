99e4ba947d5d58845c04fcc8d270a2ea215d00c7
Simonas Baltulionis <baltulionis.simonas@gmail.com>
2019-10-21 22:42:54 +0300

    Monitoring configuration list skeleton with delete button

--- GazeMonitoring/ViewModels/MonitoringConfigurationsViewModel.cs
+++ GazeMonitoring/ViewModels/MonitoringConfigurationsViewModel.cs
@@ -1,9 +1,39 @@
-﻿using GazeMonitoring.Base;
+﻿using System.Collections.ObjectModel;
+using GazeMonitoring.Base;
+using GazeMonitoring.Commands;
+using GazeMonitoring.DataAccess;
+using GazeMonitoring.Model;
 
 namespace GazeMonitoring.ViewModels
 {
     public class MonitoringConfigurationsViewModel : ViewModelBase, ISettingsSubViewModel
     {
+        private readonly IConfigurationRepository _configurationRepository;
+
+        public MonitoringConfigurationsViewModel(IConfigurationRepository configurationRepository)
+        {
+            _configurationRepository = configurationRepository;
+            MonitoringConfigurations = new ObservableCollection<MonitoringConfiguration>(_configurationRepository.Search<MonitoringConfiguration>());
+        }
+
+        public ObservableCollection<MonitoringConfiguration> MonitoringConfigurations { get; set; }
+
+        private RelayCommand<MonitoringConfiguration> _deleteCommand;
+
+
+        public RelayCommand<MonitoringConfiguration> DeleteCommand
+        {
+            get
+            {
+                return _deleteCommand
+                       ?? (_deleteCommand = new RelayCommand<MonitoringConfiguration>(
+                           monitoringConfig =>
+                           {
+                               MonitoringConfigurations.Remove(monitoringConfig);
+                           }));
+            }
+        }
+
         public ESettingsSubViewModel ESettingsSubViewModel => ESettingsSubViewModel.MonitoringConfigurationsViewModel;
     }
 }
