0edaad98966a9cb0fd7e6dbcbf506c9f585cb407
Simonas Baltulionis <s.baltulionis@cid.com>
2020-01-06 16:00:51 +0200

    Add validation for required and unique profile name

--- GazeMonitoring/ViewModels/ProfilesViewModel.cs
+++ GazeMonitoring/ViewModels/ProfilesViewModel.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using GazeMonitoring.Balloon;
 using GazeMonitoring.Base;
 using GazeMonitoring.Commands;
 using GazeMonitoring.EyeTracker.Core.Calibration;
+using GazeMonitoring.Logging;
 using GazeMonitoring.Messaging;
 using GazeMonitoring.Messaging.Messages;
 using GazeMonitoring.WindowModels;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace GazeMonitoring.ViewModels
 {
@@ -13,6 +17,7 @@ namespace GazeMonitoring.ViewModels
     {
         private readonly IMessenger _messenger;
         private readonly ICalibrationManager _calibrationManager;
+        private readonly IBalloonService _balloonService;
         private ObservableCollection<ProfileWindowModel> _profiles;
         public EMainSubViewModel SubViewModel => EMainSubViewModel.ProfilesViewModel;
 
@@ -20,6 +25,8 @@ namespace GazeMonitoring.ViewModels
         private ProfileWindowModel _selectedProfile;
         private bool _addEditProfileModeEnabled;
         private ProfileWindowModel _addEditProfileWindowModel;
+        private string _globalErrorMessage;
+        private readonly ILogger _logger;
 
         public ProfileWindowModel AddEditProfileWindowModel
         {
@@ -33,10 +40,17 @@ namespace GazeMonitoring.ViewModels
             set => SetProperty(ref _addEditProfileModeEnabled, value);
         }
 
-        public ProfilesViewModel(IMessenger messenger, ICalibrationManager calibrationManager)
+        public string GlobalErrorMessage
+        {
+            get => _globalErrorMessage;
+            set => SetProperty(ref _globalErrorMessage, value);
+        }
+
+        public ProfilesViewModel(IMessenger messenger, ICalibrationManager calibrationManager, ILoggerFactory loggerFactory, IBalloonService balloonService)
         {
             _messenger = messenger;
             _calibrationManager = calibrationManager;
+            _balloonService = balloonService;
             Profiles = new ObservableCollection<ProfileWindowModel>(_calibrationManager.GetProfilesAsync().Result
                 .Select(o => new ProfileWindowModel {Name = o}));
             _currentProfile = _calibrationManager.GetCurrentProfileAsync().Result;
@@ -52,6 +66,7 @@ namespace GazeMonitoring.ViewModels
             });
             AddEditProfileWindowModel = new ProfileWindowModel();
             AddEditProfileModeEnabled = false;
+            _logger = loggerFactory.GetLogger(typeof(ProfilesViewModel));
         }
 
         public RelayCommand BackCommand => new RelayCommand(() =>
@@ -61,27 +76,70 @@ namespace GazeMonitoring.ViewModels
 
         public AwaitableDelegateCommand<ProfileWindowModel> CalibrateCommand => new AwaitableDelegateCommand<ProfileWindowModel>(async (profile) =>
             {
-                await _calibrationManager.Calibrate(profile.Name);
+                try
+                {
+                    await _calibrationManager.Calibrate(profile.Name);
+                }
+                catch (Exception ex)
+                {
+                    _balloonService.ShowBalloonTip("Error", "Failed to calibrate a profile.", BalloonIcon.Error);
+                    _logger.Error($"Failed to calibrate the profile with name: '{profile.Name}'. Ex: {ex}");
+                }
             });
 
         public AwaitableDelegateCommand<ProfileWindowModel> DeleteCommand => new AwaitableDelegateCommand<ProfileWindowModel>(async (profile) =>
         {
-            await _calibrationManager.DeleteProfileAsync(profile.Name);
-            Profiles.Remove(profile);
-            if (_currentProfile == profile.Name)
+            try
             {
-                _messenger.Send(new ShowProfilesMessage());
+                if (profile.Name == SelectedProfile.Name)
+                {
+                    GlobalErrorMessage = "Cannot deleted the selected profile.";
+                    return;
+                }
+
+                GlobalErrorMessage = "";
+                await _calibrationManager.DeleteProfileAsync(profile.Name);
+                Profiles.Remove(profile);
+                if (_currentProfile == profile.Name)
+                {
+                    _messenger.Send(new ShowProfilesMessage());
+                }
+            } catch (Exception ex)
+            {
+                _balloonService.ShowBalloonTip("Error", "Failed to delete profile.", BalloonIcon.Error);
+                _logger.Error($"Failed to save profile with name: '{AddEditProfileWindowModel.Name}'. Ex: {ex}");
             }
         });
 
         public AwaitableDelegateCommand SaveProfileCommand => new AwaitableDelegateCommand(async () =>
         {
-            await _calibrationManager.CreateProfileAsync(AddEditProfileWindowModel.Name);
-            _messenger.Send(new ShowMainNavigationMessage());
+            try
+            {
+                GlobalErrorMessage = "";
+
+                if (!IsValidProfile()) return;
+
+                var profiles = await _calibrationManager.GetProfilesAsync();
+                if (profiles.Any(o => o == AddEditProfileWindowModel.Name))
+                {
+                    // TODO: Might set error to internal AddEditProfileWindowModel that error message would appear near field.
+                    GlobalErrorMessage = "The profile with the same name already exists.";
+                    return;
+                }
+
+                await _calibrationManager.CreateProfileAsync(AddEditProfileWindowModel.Name);
+                _messenger.Send(new ShowMainNavigationMessage());
+            }
+            catch(Exception ex)
+            {
+                _balloonService.ShowBalloonTip("Error", "Failed to save profile.", BalloonIcon.Error);
+                _logger.Error($"Failed to save profile with name: '{AddEditProfileWindowModel.Name}'. Ex: {ex}");
+            }
         });
 
         public DelegateCommand AddCommand => new DelegateCommand(() =>
         {
+            GlobalErrorMessage = "";
             AddEditProfileModeEnabled = true;
             AddEditProfileWindowModel = new ProfileWindowModel();
         });
@@ -97,10 +155,24 @@ namespace GazeMonitoring.ViewModels
             get => _selectedProfile;
             set
             {
+                GlobalErrorMessage = "";
                 if (value == null) return;
                 SetProperty(ref _selectedProfile, value);
                 _calibrationManager.SetCurrentProfileAsync(_selectedProfile.Name).Wait();
             }
+        }
+
+        private bool IsValidProfile()
+        {
+            var isValid = !AddEditProfileWindowModel.HasErrors;
+
+            if (string.IsNullOrEmpty(AddEditProfileWindowModel.Name))
+            {
+                isValid = false;
+                AddEditProfileWindowModel.Name = null;
+            }
+
+            return isValid;
         }
     }
 }
--- GazeMonitoring/WindowModels/ProfileWindowModel.cs
+++ GazeMonitoring/WindowModels/ProfileWindowModel.cs
@@ -1,11 +1,14 @@
-﻿using GazeMonitoring.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using GazeMonitoring.Base;
 
 namespace GazeMonitoring.WindowModels
 {
-    public class ProfileWindowModel : ViewModelBase
+    public class ProfileWindowModel : ValidatableBindableBase
     {
         private string _name;
 
+        [Required]
+        [StringLength(20, MinimumLength = 1)]
         public string Name
         {
             get => _name;
